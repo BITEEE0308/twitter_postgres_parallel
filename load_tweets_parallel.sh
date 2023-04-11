3ea1a2da132b716bac8cba20af96127f31b41356
Judith Zhao <judithzhao@outlook.com>
2023-04-11 10:44:16 -0700

    Move Time Position

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,14 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-echo "$files" | time parallel bash load_denormalized.sh
+time echo "$files" | parallel bash load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:51298/ --inputs
+time echo "$files" | parallel python3 -u load_tweets.py --db=postgresql://postgres:pass@localhost:51298/ --inputs
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-echo "$files" | time parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:12235/ --inputs
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:12235/ --inputs
