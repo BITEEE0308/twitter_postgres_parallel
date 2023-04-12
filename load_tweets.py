835a4a1e945ac6d236b9e8d2484d48a828ee91b8
Judith Zhao <judithzhao@outlook.com>
2023-04-11 17:30:59 -0700

    Test Batch_Parallel

--- load_tweets.py
+++ load_tweets.py
@@ -103,11 +103,21 @@ def insert_tweet(connection,tweet):
     if res.first() is not None:
         return
 
-    connection.commit()
     # insert tweet within a transaction;
     # this ensures that a tweet does not get "partially" loaded
     with connection.begin() as trans:
 
+    
+        sql=sqlalchemy.sql.text('''
+        SELECT id_tweets 
+        FROM tweets
+        WHERE id_tweets = :id_tweets
+        ''')
+        res = connection.execute(sql,{
+            'id_tweets':tweet['id'],
+            })
+        if res.first() is not None:
+            return
         ########################################
         # insert into the users table
         ########################################
